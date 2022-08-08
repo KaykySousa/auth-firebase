b0e2f10ee8b9d0142ae8d66fb4a232d496ca66da
KaykySousa <kaykysousa321@gmail.com>
2022-08-08 09:11:27 -0300

    second commit

--- app/src/main/java/com/example/authentication/MainActivity.kt
+++ app/src/main/java/com/example/authentication/MainActivity.kt
@@ -1,11 +1,63 @@
 package com.example.authentication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = Firebase.auth
+
+        val txtRegisterLink: TextView = findViewById(R.id.txtRegisterLink)
+
+        txtRegisterLink.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        val loginButton: Button = findViewById(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+            handleLogin()
+        }
+    }
+
+    private fun handleLogin() {
+        val emailInput: EditText = findViewById(R.id.emailInput)
+        val passwordInput: EditText = findViewById(R.id.passwordInput)
+
+        val email: String = emailInput.text.toString()
+        val password: String = passwordInput.text.toString()
+
+        try {
+            if (emailInput.text.isEmpty() || passwordInput.text.isEmpty()) {
+                throw Exception("Preencha todos os dados")
+            }
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "Tá lá", Toast.LENGTH_SHORT).show()
+                    } else {
+                        throw Exception("Deu ruim")
+                    }
+                }
+
+        } catch (e: Exception) {
+            Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+        }
     }
 }
